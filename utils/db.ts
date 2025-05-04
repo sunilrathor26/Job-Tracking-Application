d01a4a3672a7f6a448fa2f922e897330f8c017c9
SunilRathor <sunilrathor2699@gmail.com>
2025-05-04 21:10:41 +0530

    job tracking application

--- utils/db.ts
+++ utils/db.ts
@@ -16,7 +16,8 @@
 
 // if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
